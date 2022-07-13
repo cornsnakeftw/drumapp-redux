ffc7f338aab55acd12778f74c990bf1f04b56977
marianne <marianne@besquare.com.my>
2022-07-13 17:38:55 +0800

    update

--- src/Components/MainContainer/index.tsx
+++ src/Components/MainContainer/index.tsx
@@ -4,19 +4,8 @@ import ScoreContainer from "../ScoreContainer";
 import SequenceContainer from "../SequenceContainer";
 import TargetContainer from "../TargetContainer";
 import "./main-container.css";
-import { useStores } from "../../stores";
 import { observer } from "mobx-react-lite";
-
-const generateTestBeat = () => {
-  const test_keys = ["a", "s", "d", "f", "g"];
-  const generated_array = [];
-  for (let index = 0; index < 10; index++) {
-    generated_array.push(...test_keys);
-  }
-  return generated_array;
-};
-
-const target_keys: string[] = generateTestBeat();
+import { useStores } from "../../stores";
 
 type RecordItem = {
   key: string;
@@ -27,8 +16,16 @@ const MainContainer = () => {
   const { app_store, main_store } = useStores();
 
   React.useEffect(() => {
+    main_store.generateTestBeat();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  React.useEffect(() => {
     main_store.setCurrentIndex(0);
     main_store.setScore(0);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [app_store.has_game_started]);
 
   React.useEffect(() => {
@@ -36,6 +33,7 @@ const MainContainer = () => {
       main_store.setRecordArray([]);
       main_store.setStartTime(Date.now());
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [app_store.is_recording]);
 
   React.useEffect(() => {
@@ -49,50 +47,13 @@ const MainContainer = () => {
 
       Promise.all(promises).then(() => app_store.setIsPlayingBack(false));
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [app_store.is_playing_back, main_store.recordArray]);
-
-  const playRecordedItem = (record_item: RecordItem) => {
-    return new Promise<void>((resolve) => {
-      setTimeout(async () => {
-        main_store.setActiveKey(record_item.key);
-        await playSound(record_item.key);
-        main_store.setActiveKey(undefined);
-
-        resolve();
-      }, record_item.time);
-    });
-  };
-
-  const onKeyPress = (pressed_key: string) => {
-    main_store.setActiveKey(pressed_key);
-
-    if (!app_store.has_game_started && !app_store.is_recording) return;
-
-    if (app_store.is_recording) {
-      main_store.setRecordArray([
-        ...main_store.recordArray,
-        { key: pressed_key, time: Date.now() - main_store.startTime },
-      ]);
-    } else {
-      if (main_store.current_index + 1 <= target_keys.length) {
-        if (pressed_key === target_keys[main_store.current_index]) {
-          main_store.setScore(main_store.score + 1);
-          main_store.setCurrentIndex(main_store.current_index + 1);
-        } else {
-          main_store.setScore(main_store.score - 1);
-        }
-      }
-
-      if (main_store.current_index + 1 >= target_keys.length) {
-        alert("Game is complete!");
-      }
-    }
-  };
 
   return (
     <div className="main-container">
       <ScoreContainer />
-      <SequenceContainer target_keys={target_keys} />
+      <SequenceContainer target_keys={main_store.target_keys} />
       <TargetContainer onKeyPress={onKeyPress} />
     </div>
   );
--- src/stores/main-store.js
+++ src/stores/main-store.js
@@ -1,4 +1,5 @@
 import { action, observable } from "mobx";
+import { playSound } from "../Helpers/sounds";
 
 export default class MainStore {
   constructor(root_store) {
@@ -10,6 +11,102 @@ export default class MainStore {
   @observable recordArray = [];
   @observable score = 0;
   @observable startTime = 0;
+
+  target_keys = [];
+
+  @action.bound
+  generateTestBeat() {
+    const test_keys = ["a", "s", "d", "f", "g"];
+    const generated_array = [];
+    for (let index = 0; index < 10; index++) {
+      generated_array.push(...test_keys);
+    }
+    this.target_keys = generated_array;
+    return this.target_keys;
+  }
+
+  @action.bound
+  onKeyPress(pressed_key) {
+    const { app_store } = this.root_store;
+
+    this.setActiveKey(pressed_key);
+
+    if (!app_store.has_game_started && !app_store.is_recording) return;
+
+    if (app_store.is_recording) {
+      this.setRecordArray([
+        ...this.recordArray,
+        { key: pressed_key, time: Date.now() - this.startTime },
+      ]);
+    } else {
+      if (this.current_index + 1 <= this.target_keys.length) {
+        if (pressed_key === this.target_keys[this.current_index]) {
+          this.setScore(this.score + 1);
+          this.setCurrentIndex(this.current_index + 1);
+        } else {
+          this.setScore(this.score - 1);
+        }
+      }
+
+      if (this.current_index + 1 >= this.target_keys.length) {
+        alert("Game is complete!");
+      }
+    }
+  }
+
+  @action.bound
+  onKeyPress = (pressed_key) => {
+    const { app_store } = this.root_store;
+    this.setActiveKey(pressed_key);
+
+    if (!app_store.has_game_started && !app_store.is_recording) return;
+
+    if (app_store.is_recording) {
+      this.setRecordArray([
+        ...this.recordArray,
+        { key: pressed_key, time: Date.now() - this.startTime },
+      ]);
+    } else {
+      if (this.current_index + 1 <= this.target_keys.length) {
+        if (pressed_key === this.target_keys[this.current_index]) {
+          this.setScore(this.score + 1);
+          this.setCurrentIndex(this.current_index + 1);
+        } else {
+          this.setScore(this.score - 1);
+        }
+      }
+
+      if (this.current_index + 1 >= this.target_keys.length) {
+        alert("Game is complete!");
+      }
+    }
+  };
+
+  @action.bound
+  playRecordedItem(record_item) {
+    return new Promise((resolve) => {
+      setTimeout(async () => {
+        this.setActiveKey(record_item.key);
+        await playSound(record_item.key);
+        this.setActiveKey(undefined);
+
+        resolve();
+      }, record_item.time);
+    });
+  }
+
+  @action.bound
+  playRecordedItem(record_item) {
+    return new Promise((resolve) => {
+      setTimeout(async () => {
+        this.setActiveKey(record_item.key);
+        await playSound(record_item.key);
+        this.setActiveKey(undefined);
+
+        resolve();
+      }, record_item.time);
+    });
+  }
 
   @action.bound
   setActiveKey(active_key) {
